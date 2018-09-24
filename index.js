423666cf1dc3f1e5cb98661727026d450a8fd663
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-09-24 18:02:35 +0200

    Add 404.png, Change index.js

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
     	fs.readFile('./index.html', 'utf-8', function(err, data) {
 	    response.write(data);
         response.end(); 
 		});
     } else {
-            response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-            response.end();
+        response.setHeader("Content-Type", "image/gif");
+        var img = fs.readFileSync('./404.png');
+        response.statusCode = 404;
+        response.write(img);
+        response.end();
+
     }
 });
 
